273ff105701dbeb5442f41c5b8d5d2f9d8ef5bd6
ictrobot <ictrobot@outlook.com>
2023-12-22 07:09:03 +0000

    Optimize 2023 Day 22
    Storing bricks at each Z in Settle is much faster than checking every brick

--- internal/aoc2023/day22/day22.go
+++ internal/aoc2023/day22/day22.go
@@ -12,18 +12,22 @@ var Example string
 
 type Day22 struct {
 	bricks []brick
+	maxZ   uint
 }
 
-type brick [2]vec.I3[int]
+type brick [2]vec.I3[uint]
 
 func (d *Day22) Parse(input string) {
-	ints := parse.ExtractInts(input)
+	ints := parse.ExtractUints(input)
+
 	d.bricks = nil
+	d.maxZ = 0
 	for i := 0; i < len(ints)-5; i += 6 {
 		d.bricks = append(d.bricks, brick{
 			{ints[i+0], ints[i+1], ints[i+2]},
 			{ints[i+3], ints[i+4], ints[i+5]},
 		})
+		d.maxZ = max(d.maxZ, ints[i+5])
 	}
 
 	// real input isn't sorted by z ascending unlike example!
@@ -74,15 +78,16 @@ func (d *Day22) Settle() ([]brick, [][]int, [][]int) {
 	bricks := slices.Clone(d.bricks)
 	supportedBy := make([][]int, len(d.bricks))
 	supports := make([][]int, len(d.bricks))
+	atZ := make([][]int, d.maxZ)
 
 	for i := range bricks {
 		for bricks[i][0].Z > 1 && bricks[i][1].Z > 1 {
 			newBrick := brick{
-				bricks[i][0].Add(vec.I3[int]{0, 0, -1}),
-				bricks[i][1].Add(vec.I3[int]{0, 0, -1}),
+				bricks[i][0].Sub(vec.I3[uint]{0, 0, 1}),
+				bricks[i][1].Sub(vec.I3[uint]{0, 0, 1}),
 			}
 
-			for j := range bricks[:i] {
+			for _, j := range atZ[newBrick[0].Z-1] {
 				if bricks[j].OverlapsWith(newBrick) {
 					supportedBy[i] = append(supportedBy[i], j)
 					supports[j] = append(supports[j], i)
@@ -94,6 +99,8 @@ func (d *Day22) Settle() ([]brick, [][]int, [][]int) {
 
 			bricks[i] = newBrick
 		}
+
+		atZ[bricks[i][1].Z-1] = append(atZ[bricks[i][1].Z-1], i)
 	}
 
 	return bricks, supports, supportedBy
