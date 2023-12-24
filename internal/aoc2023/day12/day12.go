7d0937ac986507da3d9f3298be91e6e52c050d76
ictrobot <ictrobot@outlook.com>
2023-12-24 17:53:52 +0000

    Optimize 2023 Day 12
    Store minimum number of springs to match the pattern and return early if there are not enough remaining springs
    
    Drops runtime from ~14ms to ~9ms

--- internal/aoc2023/day12/day12.go
+++ internal/aoc2023/day12/day12.go
@@ -68,29 +68,38 @@ func (d *Day12) Part2() any {
 }
 
 func (r row) arrangements() int64 {
-	m := make(map[uint64]int64)
+	m := make(map[uint32]int64)
 
-	// remaining is the number of broken springs left in the current contiguous
-	// broken group. 0 means a contiguous group just ended, so the next spring
-	// must be operational. -1 is a placeholder for at least one operational
-	// spring has passed since the last broken group (so a new group can start)
-	var f func([]spring, []int, int) int64
-	f = func(springs []spring, groups []int, remaining int) int64 {
+	// currentGroup is the number of broken springs left in the current
+	// contiguous group. 0 means a group just ended, so the next spring must be
+	// operational. -1 is a placeholder for at least one operational spring has
+	// passed since the last broken group (so a new group can start)
+	var f func([]spring, []int, int, int) int64
+	f = func(springs []spring, groups []int, currentGroup, remainingGroup int) int64 {
+		if len(springs) < remainingGroup {
+			// not enough remaining springs to match group pattern
+			return 0
+		}
 
 		// process any known springs
 		for len(springs) > 0 && (springs[0] == operational || springs[0] == broken) {
 			s := springs[0]
 			springs = springs[1:]
 
-			if s == operational && remaining <= 0 {
-				// valid operational spring
-				remaining = -1
-			} else if s == broken && remaining > 0 {
+			if s == operational && currentGroup == -1 {
+				// operational spring not immediately after broken group
+			} else if s == operational && currentGroup == 0 {
+				// operational spring immediately after broken group
+				currentGroup = -1
+				remainingGroup--
+			} else if s == broken && currentGroup > 0 {
 				// in contiguous broken group
-				remaining--
-			} else if s == broken && remaining == -1 && len(groups) > 0 {
+				currentGroup--
+				remainingGroup--
+			} else if s == broken && currentGroup == -1 && len(groups) > 0 {
 				// start next broken group
-				groups, remaining = groups[1:], groups[0]-1
+				groups, currentGroup = groups[1:], groups[0]-1
+				remainingGroup--
 			} else {
 				// invalid combination
 				return 0
@@ -99,35 +108,35 @@ func (r row) arrangements() int64 {
 
 		// if there are remaining springs, the first must be unknown
 		if len(springs) > 0 {
-			if remaining > 0 {
+			if currentGroup > 0 || (currentGroup == -1 && len(springs) == remainingGroup) {
 				// unknown spring must be broken as inside contiguous broken group
+				// or the number of remaining springs equals the minimum to match
+				// the pattern
 				springs[0] = broken
-				result := f(springs, groups, remaining)
+				result := f(springs, groups, currentGroup, remainingGroup)
 				springs[0] = unknown
 				return result
 			}
 
-			if remaining == 0 {
+			if currentGroup == 0 {
 				// unknown spring must be operational as immediately after broken group
 				springs[0] = operational
-				result := f(springs, groups, remaining)
+				result := f(springs, groups, currentGroup, remainingGroup)
 				springs[0] = unknown
 				return result
 			}
 
 			// unknown spring could be either - sum both possibilities and cache
 			// use a packed integer for the map key for faster map performance
-			key := (uint64(uint16(len(springs))) << 32) +
-				(uint64(uint16(len(groups))) << 16) +
-				(uint64(uint16(remaining)))
+			key := (uint32(uint16(len(springs))) << 16) + uint32(uint16(remainingGroup))
 			if result, ok := m[key]; ok {
 				return result
 			}
 
 			springs[0] = broken
-			result := f(springs, groups, remaining)
+			result := f(springs, groups, currentGroup, remainingGroup)
 			springs[0] = operational
-			result += f(springs, groups, remaining)
+			result += f(springs, groups, currentGroup, remainingGroup)
 			springs[0] = unknown
 
 			m[key] = result
@@ -135,12 +144,24 @@ func (r row) arrangements() int64 {
 		}
 
 		// no more springs to process
-		if len(groups) > 0 || remaining > 0 {
+		if remainingGroup > 0 {
 			// invalid combination, expected more broken springs
 			return 0
 		}
 		return 1
 	}
 
-	return f(slices.Clone(r.springs), r.groups, -1)
+	// calculate the minimum number of springs required to match the broken
+	// groups pattern
+	totalGroupSprings := 0
+	for _, g := range r.groups {
+		// all groups need to be followed by an operational spring so +1
+		totalGroupSprings += g + 1
+	}
+	if totalGroupSprings > 0 {
+		// last group doesn't need to be followed by operational spring
+		totalGroupSprings--
+	}
+
+	return f(slices.Clone(r.springs), r.groups, -1, totalGroupSprings)
 }
